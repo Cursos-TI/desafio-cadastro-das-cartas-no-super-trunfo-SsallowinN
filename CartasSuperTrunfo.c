4c6db90eae5f7c347898a0a2adcdc4abeae603f0
SsallowinN <washingtoneliasaraujo@gmail.com>
2025-08-06 22:22:02 +0000

    FIZ O DESAFIO NIVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,85 +1,91 @@
 #include <stdio.h>
 
 int main() {
-    // ------- INICIANDO AS VARIAVEIS DA CARTA 1 ---------
-    char ESTADO_1, COD_CARTA_1[4], CD_NOME_1[50];
-    int POPULACAO_1, PTT_1;
-    float AREA_KM1, PIB_1;
+    //---------------------  DECLARANDO AS VARIÁVEIS CARTA 1 -----------------------
+    char estado1, cod_carta1[4], cd_nome1[50];
+    int populacao1, np_turisticos1;
+    float area1, PIB1;
 
-    // ------- INICIANDO AS VARIAVEIS DA CARTA 2 ---------
-    char ESTADO_2, COD_CARTA_2[4], CD_NOME_2[50];
-    int POPULACAO_2, PTT_2;
-    float AREA_KM2, PIB_2;
+    //---------------------  DECLARANDO AS VARIÁVEIS CARTA 2 -----------------------
+    char estado2, cod_carta2[4], cd_nome2[50];
+    int populacao2, np_turisticos2;
+    float area2, PIB2;
 
-    // ------- INICIANDO UMA INTERAÇÃO COM O USUARIO ---------
-    printf("BEM_VINDO AO SUPER TRUNFO!!!\n");
-    printf("VAMOS CRIAR SUA PRIMEIRA CARTA!!\n\n");
+    //---------------------  INSERINDO OS DADOS DA CARTA 1 -----------------------
+    printf("Escolha uma letra que represente o estado dessa cidade (A a H): ");
+    scanf(" %c", &estado1); 
 
-    // ------- PEGANDO A ENTRADA DO USUARIO PARA A CARTA 1 -----
-    printf("Escolha uma letra que represente o estado dessa cidade, você pode escolher de (A a H)\n");
-    scanf(" %c", &ESTADO_1);  // ESPAÇO antes do %c para evitar lixo
+    printf("Escolha o codigo dessa carta usando a letra que voce escolheu e o numero (ex:A01): ");
+    scanf(" %s", cod_carta1);
 
-    printf("Escolha o codigo dessa carta usando a letra que você escolheu e o numero (ex:A01)\n");
-    scanf("%s", COD_CARTA_1);
+    printf("Escolha o nome da cidade dessa carta (ex:Barcarena): ");
+    scanf(" %[^\n]", cd_nome1);
 
-    printf("Escolha o nome da cidade dessa carta (ex:Barcarena)\n");
-    scanf("%s", CD_NOME_1);
+    printf("Escolha o numero de habitantes dessa cidade (ex:20000): ");
+    scanf("%d", &populacao1);
 
-    printf("Escolha o numero de habitantes dessa cidade (ex:20000)\n");
-    scanf("%d", &POPULACAO_1);
+    printf("Escolha o numero de pontos turisticos dessa cidade (ex:10): ");
+    scanf("%d", &np_turisticos1);
 
-    printf("Escolha o numero de pontos turisticos dessa cidade (ex:10)\n");
-    scanf("%d", &PTT_1);
+    printf("Escolha a area em Km² dessa cidade (ex:10.000): ");
+    scanf("%f", &area1);
 
-    printf("Escolha a aréa em Km² dessa cidade (ex:10.000)\n");
-    scanf("%f", &AREA_KM1);
-
-    printf("Escolha o PIB dessa cidade (ex:10.000.000.00)\n");
-    scanf("%f", &PIB_1);
+    printf("Escolha o PIB dessa cidade (ex:10.000.000.00): ");
+    scanf("%f", &PIB1);
 
     printf("PERFEITO! AGORA VAMOS PARA SUA SEGUNDA CARTA!!\n");
 
-    // ------- PEGANDO A ENTRADA DO USUARIO PARA A CARTA 2 -----
-    printf("Escolha uma letra que represente o estado dessa cidade, você pode escolher de (A a H)\n");
-    scanf(" %c", &ESTADO_2);  // ESPAÇO antes do %c para evitar lixo
+    //---------------------  INSERINDO OS DADOS DA CARTA 2 -----------------------
+    printf("Escolha uma letra que represente o estado dessa cidade (A a H): ");
+    scanf(" %c", &estado2);
 
-    printf("Escolha o codigo dessa carta usando a letra que você escolheu e o numero (ex:A01)\n");
-    scanf("%s", COD_CARTA_2);
+    printf("Escolha o codigo dessa carta usando a letra que voce escolheu e o numero (ex:A01): ");
+    scanf(" %s", cod_carta2);
 
-    printf("Escolha o nome da cidade dessa carta (ex:Barcarena)\n");
-    scanf("%s", CD_NOME_2);
+    printf("Escolha o nome da cidade dessa carta (ex:Barcarena): ");
+    scanf(" %[^\n]", cd_nome2); // <-- ESPAÇO ANTES IMPORTANTE
 
-    printf("Escolha o numero de habitantes dessa cidade (ex:20000)\n");
-    scanf("%d", &POPULACAO_2);
+    printf("Escolha o numero de habitantes dessa cidade (ex:20000): ");
+    scanf("%d", &populacao2);
 
-    printf("Escolha o numero de pontos turisticos dessa cidade (ex:10)\n");
-    scanf("%d", &PTT_2);
+    printf("Escolha o numero de pontos turisticos dessa cidade (ex:10): ");
+    scanf("%d", &np_turisticos2);
 
-    printf("Escolha a aréa em Km² dessa cidade (ex:10.000)\n");
-    scanf("%f", &AREA_KM2);
+    printf("Escolha a area em Km² dessa cidade (ex:10.000): ");
+    scanf("%f", &area2);
 
-    printf("Escolha o PIB dessa cidade (ex:10.000.000.00)\n");
-    scanf("%f", &PIB_2);
+    printf("Escolha o PIB dessa cidade (ex:10.000.000.00): ");
+    scanf("%f", &PIB2);
 
-    // ------------- SAIDA DE DADOS DA CARTA 1 ---------------
+    // ----------- CALCULANDO A DENSIDADE POPULACIONAL E PIB PER CAPITA -------------
+    float dp1 = populacao1 / area1;
+    float pib_pc1 = PIB1 / populacao1;
+
+    float dp2 = populacao2 / area2;
+    float pib_pc2 = PIB2 / populacao2;
+
+    // --------------  MOSTRANDO AS CARTAS -------------
     printf("\n--------- CARTA 1 ---------\n");
-    printf("Estado: %c\n", ESTADO_1);
-    printf("Codigo: %s\n", COD_CARTA_1);
-    printf("Nome da cidade: %s\n", CD_NOME_1);
-    printf("População: %d\n", POPULACAO_1);
-    printf("Pontos turisticos: %d\n", PTT_1);
-    printf("Área: %.2f Km²\n", AREA_KM1);
-    printf("PIB: %.2f\n", PIB_1);
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", cod_carta1);
+    printf("Nome da cidade: %s\n", cd_nome1);
+    printf("População: %d\n", populacao1);
+    printf("Pontos turísticos: %d\n", np_turisticos1);
+    printf("Área: %.2f Km²\n", area1);
+    printf("PIB: %.2f\n", PIB1);
+    printf("Densidade populacional: %.2f\n", dp1);
+    printf("PIB per capita: %.2f\n", pib_pc1);
 
-    // ------------- SAIDA DE DADOS DA CARTA 2 ---------------
     printf("\n--------- CARTA 2 ---------\n");
-    printf("Estado: %c\n", ESTADO_2);
-    printf("Codigo: %s\n", COD_CARTA_2);
-    printf("Nome da cidade: %s\n", CD_NOME_2);
-    printf("População: %d\n", POPULACAO_2);
-    printf("Pontos turisticos: %d\n", PTT_2);
-    printf("Área: %.2f Km²\n", AREA_KM2);
-    printf("PIB: %.2f\n", PIB_2);
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", cod_carta2);
+    printf("Nome da cidade: %s\n", cd_nome2);
+    printf("População: %d\n", populacao2);
+    printf("Pontos turísticos: %d\n", np_turisticos2);
+    printf("Área: %.2f Km²\n", area2);
+    printf("PIB: %.2f\n", PIB2);
+    printf("Densidade populacional: %.2f\n", dp2);
+    printf("PIB per capita: %.2f\n", pib_pc2);
 
     return 0;
 }
